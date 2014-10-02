0e21406112417f335330c47cbdd5fb1806a02158
root <wl_joep@hotmail.com>
2014-10-02 15:16:32 -0500

    final updater?

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ SERVER_INIT=server/index.js
 cd $GIT_DIR
 sudo git pull $REPOSITORY | grep "up-to-date"
 UPDATE_TO_DATE=$?
-if [ "$UP_TO_DATE" = "1" ]; then
- screen -X -S $SCREEN_SESSION quit
- screen -d -m -S $SCREEN_SESSION
- screen -X -S $SCREEN_SESSION sudo nodejs $SERVER_INIT
-else
+#if [ "$UP_TO_DATE" = "1" ]; then
+ sudo screen -X -S $SCREEN_SESSION quit
+ sudo screen -d -m -S $SCREEN_SESSION
+ sudo screen -X -S $SCREEN_SESSION stuff "sudo nodejs $SERVER_INIT\n"
+#else
  echo up to date
-fi
+#fi
