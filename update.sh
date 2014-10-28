89cf68ca7b886705cb5b6e022ae9859b7f02f390
joel <wl_joep@hotmail.com>
2014-10-28 22:39:17 +0100

    Update update.sh for better server interaction

--- update.sh
+++ update.sh
@@ -2,14 +2,15 @@
 REPOSITORY=https://github.com/jjjvdberg/CoffeeApp.git
 GIT_DIR=/var/www/coffeeapp
 SCREEN_SESSION=nodejs_session
-SERVER_INIT=server/index.js
+SERVER_DIR=server
+SERVER_INIT=index.js
 cd $GIT_DIR
 sudo git pull $REPOSITORY | grep "up-to-date"
 UP_TO_DATE=$?
 if [ "$UP_TO_DATE" = "1" ]; then
  sudo screen -X -S $SCREEN_SESSION quit
  sudo screen -d -m -S $SCREEN_SESSION
- sudo screen -X -S $SCREEN_SESSION stuff "sudo nodejs $SERVER_INIT\n"
+ sudo screen -X -S $SCREEN_SESSION stuff "cd $SERVER_DIR && sudo nodejs $SERVER_INIT\n"
 else
  echo up to date
 fi
